ed7927beb43492000aabaa60f71fe04cbaf1f8bb
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-03-08 13:41:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 修改机器名称
 sed -i 's/ImmortalWrt/OpenWrt-R2s/g' package/base-files/files/bin/config_generate
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-# sed -i "s/OpenWrt /Build by Blacknesswing | OpenWrt ctcgfw /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/# /Build by Blacknesswing | OpenWrt ctcgfw /g" package/lean/default-settings/files/zzz-default-settings
 
 #SWAP LAN WAN（满足千兆场景，可选
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
