1cae2c2b1bdbde9a6f279db37f3682597256a01e
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-03-08 22:21:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/OpenWrt-R2s/g' package/base-files/files/bin/config_generate
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "/openwrt_luci/ { s/snapshots/releases\/18.06.9/g; } /openwrt_luci/ { s/snapshots/releases\/18.06.9/g|Build by Blacknesswing; } /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/openwrt_luci/ { s/snapshots/releases\/18.06.9/g; } /openwrt_luci/ { s/snapshots/releases\/18.06.9/g|Build by Blacknesswing; }/g" package/lean/default-settings/files/zzz-default-settings
 
 #SWAP LAN WAN（满足千兆场景，可选
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
