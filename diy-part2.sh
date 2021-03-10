f8e604430d370847c0342308a376386f08e09473
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-03-10 14:46:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 修改机器名称
 sed -i 's/ImmortalWrt/OpenWrt-R2s/g' package/base-files/files/bin/config_generate
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "/minidlna_status.htm/a\sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
-sed -i "/minidlna_status.htm/a\echo "DISTRIB_DESCRIPTION='|Build_Date@blacknesswing'" >> /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
+# sed -i "/minidlna_status.htm/a\sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
+# sed -i "/minidlna_status.htm/a\echo "DISTRIB_DESCRIPTION='|Build_Date@blacknesswing'" >> /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
 
 #SWAP LAN WAN（满足千兆场景，可选
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
