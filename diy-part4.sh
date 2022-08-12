61881095d2072a49faacdf07aff79adc5132f5b1
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2022-08-12 22:52:49 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -31,3 +31,4 @@ sed -i "s/OpenWrt /Build by Blacknesswing | OpenWrt Lean /g" package/lean/defaul
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -ri '/^\s*TARGET_DEVICES\s.+?(fastrhino_r66s|firefly_station-p2)/d' target/linux/rockchip/image/armv8.mk
