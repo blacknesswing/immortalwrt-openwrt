58d5998d1a87d05ad9233a244e6c80715b7d1613
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-02-08 21:27:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,28 +15,8 @@
 #添加订阅源
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-rm -rf package/lean/luci-app-passwall2
-rm -rf package/lean/passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/custom/luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/custom/passwall-packages
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"#oled
+# miomo
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
-# git clone https://github.com/NateLol/luci-app-oled package/luci-app-oled
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# argon-主题
-# rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
-# 京东签到
-# git clone https://github.com/jerrykuku/node-request package/node-request
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
-# 微信推送
-# git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan 
-# luci-app-cpufreq
-# rm -rf package/lean/luci-app-cpufreq && svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/luci-app-cpufreq package/lean/luci-app-cpufreq
-# git clone https://github.com/281677160/openwrt-package package/luci-app-cpulimit
-# Vssr
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
