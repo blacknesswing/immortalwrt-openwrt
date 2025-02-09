1bc989b9cfc7b0b0d03a4a2d031f5f9e01cb31c5
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-02-10 06:32:44 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -14,7 +14,7 @@
  sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 # 将构建日期添加到概览页面
-sed -i "s/%C/($(date +"%Y-%m-%d"))/g" package/base-files/files/usr/lib/os-release
+# sed -i "s/%C/($(date +"%Y-%m-%d"))/g" package/base-files/files/usr/lib/os-release
 
 # 修改系统版本（界面显示）
 VERSION_FILE="immortalwrt/version"
